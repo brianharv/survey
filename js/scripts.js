31f4d9beffe5ac6b389aaeca79c13dbda4c9bb62
Brian Harvey <brian.harv3y@gmail.com>
2020-08-12 21:20:51 -0700

    add js methods for Select Box, Radio Button, Date, and Color input forms

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+//Everything Below this line is user interface logic:
+$(document).ready(function() {
+  $("#userName").submit(function(event) {
+    event.preventDefault();
+    const userFirstName = $("input#firstname").val();
+    const userLastName = $("input#lastname").val();
+    const userFavFood = $("input#favfood").val();
+    
+    $("#return").show();  
+  });
+  const userMusic = $("#music").val();
+  
+  const how = $("input:radio[name=how]:checked").val();
+
+  const dateOfBirth =$("#born").val();
+
+  const favoriteColor = $("#color").val();
+
+});
